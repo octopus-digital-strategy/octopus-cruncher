bbc7186141a78afe8992f1aaeafd282ebaf6d81e
awesomepage <Ceiba.72>
2015-10-22 21:48:41 -0500

    Added custom options page template

--- src/OptionsPage.php
+++ src/OptionsPage.php
@@ -11,7 +11,6 @@ use WPExpress\Abstractions\SettingsPage;
 use WPExpress\UI\HTML\Tags;
 
 
-
 class OptionsPage extends SettingsPage
 {
 
@@ -19,12 +18,101 @@ class OptionsPage extends SettingsPage
     {
         // Invoke the paren constructor
         parent::__construct( 'Performance Enhancements', 'manage_options' );
-        $this->registerPage();
 
-        // Add fields
-        $this->addOptionField( Tags::textField('test', array( 'id' => 'test', 'class' => 'form-control' ) )  )
-            ->addOptionField(  Tags::textField('test', array( 'id' => 'test', 'class' => 'form-control' ) )  );
+        $this->getNodeInformation();
+        // plugin_basename(__FILE__)
+        $this->setCustomTemplatePath( plugin_dir_path(__FILE__) . "../resources/templates" );
 
+        $description = __('This plugin requires node installed on the server.', 'octopus');
+
+        $this->registerPage()->setPageDescription($description)->registerCustomFields();
+
+        // Customize context
+        add_filter( 'wpExpressSettingsPageContext', array($this, 'customizeContext') );
+    }
+
+
+    private function getNodeInformation()
+    {
+        $this->nodePath = exec('node -v');
+
+    }
+
+
+    private function registerCustomFields()
+    {
+        // Custom Bundle Name
+        $this->addMetaField(
+            'nodePath',
+            __( 'Bundle Name', 'octopus' ),
+            'text', 'node-settings',
+            array(
+                'disabled' => true,
+                'value' => 'css-bundle.min.css',
+            ) );
+
+
+        // Persist all the registerd styles
+        // TODO: Run only once or on demand
+        add_action('wp_head', array($this, 'persistRegisteredStyles'), 99);
+        // Enqueue CSS Bundle
+        if( $this->getCSSBundlePath() != false ){
+            add_action( 'wp_head', array($this, 'enqueueCSSBundle'), 1 );
+        }
+
+        $WPStyles = new \WP_Styles();
+
+        $styles = $this->getRegisteredStyles();
+
+        foreach( $styles as $key => $style ){
+            wp_dequeue_style( $key );
+            $this->addMetaField( $key, "Style {$key}", 'checkbox', 'styles' );
+        }
+
+
+        return $this;
+    }
+
+    public function persistRegisteredStyles()
+    {
+        global $wp_styles;
+        update_option( "{$this->settingsPrefix}registeredStyles", $wp_styles->registered );
+    }
+
+    public function getRegisteredStyles()
+    {
+        return get_option( "{$this->settingsPrefix}registeredStyles", array() );
+    }
+
+    public function getCSSBundlePath()
+    {
+        $bundlePath = false;
+        // TODO: get path to Bundle
+        return $bundlePath;
+    }
+
+    public function enqueueCSSBundle()
+    {
+        global $wp_styles;
+
+        // TODO: getCompressedStyles and dequeue them
+        // TODO: enqueue CSS Bundle
+
+    }
+
+    public function customizeContext($context)
+    {
+        $context['styles'] = array();
+
+        foreach( $context['fields'] as $index => $field ){
+
+            if( isset( $field->properties['group'] ) && $field->properties['group'] == 'styles' ){
+                unset($context['fields'][$index]);
+                $context['styles'][] = $field;
+            }
+        }
+
+        return $context;
     }
 
 }
